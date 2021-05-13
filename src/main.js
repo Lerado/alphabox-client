340da7a4272457e0f51510ff31b94e4c5f34d25f
Darel Ngogang <darel.n@intekor.ca>
2021-05-13 19:41:57 +0100

    Setup Vue 3, Vue router, Vuex, Tailwind CSS and Equal vue components library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
+
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+import router from './router'
+
+
+import store from './store'
+
+
+import Equal from 'equal-vue'
+import 'equal-vue/dist/style.css'
+
+
+import 'tailwindcss/tailwind.css'
+
+
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(router)
+    .use(Equal)
+    .mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      screens: {
+        'dark': {'raw': '(prefers-color-scheme: dark)'},
+        // => @media (prefers-color-scheme: dark) { ... }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
