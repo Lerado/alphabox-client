0be2e341a04d1a1c00f91656ad3da45693e9421e
Darel Ngogang <darel.n@intekor.ca>
2021-05-14 15:42:38 +0100

    Add comments to main.js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
 import { createApp } from 'vue'
 
-
+// Import main component 
 import App from './App.vue'
 
-
+// Vue router config file
 import router from './router'
 
-
+// Vuex store management
 import store from './store'
 
-
+// Equal vue components library
 import Equal from 'equal-vue'
 import 'equal-vue/dist/style.css'
 
-
+// Tailwind CSS
 import 'tailwindcss/tailwind.css'
 
-
+// Mount app in main entry point and load plugins
 createApp(App)
     .use(store)
     .use(router)
